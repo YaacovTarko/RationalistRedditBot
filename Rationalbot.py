684fb04b49c62327199da786380aa7ba5f9bb97d
Yaacov Tarko <yaacov.tarko@gmail.com>
2016-08-15 22:06:07 -0700

    done with RSS, still need to add Reddit integration

--- Rationalbot.py
+++ Rationalbot.py
@@ -5,8 +5,9 @@ import praw           #http://pythonforengineers.com/build-a-reddit-bot-part-1/
 from dateutil.parser import *   #parse rss dates
 import datetime       #https://docs.python.org/3/library/datetime.html#timedelta-objects
 
-
 now = datetime.datetime.now()
+now += datetime.timedelta(hours=7) #account for the fact that I'm in the pacific timezone, and the dates provided by RSS are in GMT. 
+#If you're running this script somewhere else, change time accordingly
 
 class submission:
 	def __init__(self, link, title):
@@ -22,36 +23,39 @@ links_to_submit = []
 
 for blog_url in blogs_to_check:
 	feed = feedparser.parse( blog_url )
+
 	for article in feed["items"]:
-	
+		
 		if 'title' in article:
 			print article["title"]
 
 		if 'date' in article:
 			date= article["date"]
 
-		elif 'published' in article: 			###slatestarcodex, agentyduck, waitbutwhy, shtetl optimized 
+		elif 'published' in article: 			#used for slatestarcodex, agentyduck, waitbutwhy, shtetl optimized 
 			date= article["published"]
 
 		#time formatting
-		#Get one unified format- ex: 2015-09-21 05:45:27+00:00
+		#Converts to type datetime.datetime 
 		parsed_date = parse(date)
-		print parsed_date  #data type is datetime.datetime
+		print parsed_date 
+		parsed_date_unaware = parsed_date.replace(tzinfo=None)  #strips time zone info to allow arithmetic operations with 'now'
 
 
-		##check if the article was published recently
-
-
-		links_to_submit.append([article["title"], parsed_date])
+		#check if the article was published recently
+		published_recently = (datetime.timedelta(hours=24) > (now - parsed_date_unaware))
+		if published_recently:
+			links_to_submit.append([article["title"], article["link"]])
 		print "\n"
 
-
+		print len(links_to_submit)
 		
-"""
+	"""		
 #Determines which particular RSS fields this blog is using 
-	for x in article:
-		print x
-"""
+		for x in article:
+			print x
+		print "\n"
+	"""
 #praw 
 agent_name = ("Rational_Blog_Autosubmitter 0.1")
 reddit_instance = praw.Reddit(user_agent = agent_name)
