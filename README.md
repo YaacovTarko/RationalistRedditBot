58f541b2c69732b9aa986c7b5d82af502e5ae08c
Yaacov Tarko <yaacov.tarko@gmail.com>
2016-11-13 13:18:40 -0800

    Makes the script run once instead of looping forever. Updates readme to be more informative

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # RationalistRedditBot
 A bot to autosubmit content from popular rationalist blogs to reddit.com/r/rationality
 
+Note: This script attempts to read the bot's username and password from a file called config_bot.py in the same directory as the script. To allow the bot to login, create a file called config_bot.py in the directory with the following contents:
+
+REDDIT_USERNAME = '' #YOUR USERNAME as string
+REDDIT_PASSWORD = '' # YOUR PASSWORD as string
+
+
 Note: If you're running this script outside of the Pacific timezone, you'll need to modify the code slightly as indicated in the comments
+
+
+Dependencies: 
+feedparser  https://pypi.python.org/pypi/feedparser#downloads
+praw https://praw.readthedocs.io/en/stable/
+
+To install these modules type: 
+pip install --user feedparser
+pip install --user praw
+into your command line
+
 
 TODO: 
 - authenticate via Oauth2, instead of with username and password
--- Rationalbot.py
+++ Rationalbot.py
@@ -4,7 +4,7 @@ import feedparser     # https://github.com/kurtmckee/feedparser
 import praw           #http://pythonforengineers.com/build-a-reddit-bot-part-1/
 from dateutil.parser import *   #parse rss dates
 import datetime       #https://docs.python.org/3/library/datetime.html#timedelta-objects
-import time           #flor sleep function
+#import time           #for sleep function
 from config_bot import *  #imports username and password 
 
 
@@ -16,46 +16,46 @@ reddit_instance.login(REDDIT_USERNAME, REDDIT_PASSWORD)
 
 
 
-while True:
-	now = datetime.datetime.now()
-	now += datetime.timedelta(hours=7) #this line accounts for the fact that I'm in the pacific timezone, and the dates provided by RSS are in GMT. 
-	#If you're running this script somewhere else, change time accordingly
+
+now = datetime.datetime.now()
+now += datetime.timedelta(hours=7) #this line accounts for the fact that I'm in the pacific timezone, and the dates provided by RSS are in GMT. 
+#If you're running this script somewhere else, change time accordingly
 
 
 
-	blogs_to_check = ["http://www.scottaaronson.com/blog/?feed=rss2", "http://slatestarcodex.com/feed/",\
+blogs_to_check = ["http://www.scottaaronson.com/blog/?feed=rss2", "http://slatestarcodex.com/feed/",\
  	"http://agentyduck.blogspot.com/feeds/posts/default", "http://waitbutwhy.com/feed", "https://rationalconspiracy.com/feed/"] 
- 	#add "" when it starts working again
-	links_to_submit = []
+ #add "" when it starts working again
+links_to_submit = []
 
 
 
-	for blog_url in blogs_to_check:
-		print blog_url
-		feed = feedparser.parse( blog_url )
+for blog_url in blogs_to_check:
+	print blog_url
+	feed = feedparser.parse( blog_url )
 
-		for article in feed["items"]:
+	for article in feed["items"]:
 			
-			#if 'title' in article:
-			#	print article["title"]
+		#if 'title' in article:
+		#	print article["title"]
 
-			if 'date' in article:
-				date= article["date"]
+		if 'date' in article:
+			date= article["date"]
 
-			elif 'published' in article: 			#used for slatestarcodex, agentyduck, waitbutwhy, shtetl optimized 
+		elif 'published' in article: 			#used for slatestarcodex, agentyduck, waitbutwhy, shtetl optimized 
 				date= article["published"]
 
 			#time formatting
 			#Converts to type datetime.datetime 
-			parsed_date = parse(date)
+		parsed_date = parse(date)
 
-			parsed_date_unaware = parsed_date.replace(tzinfo=None)  #strips time zone info to allow arithmetic operations with 'now'
+		parsed_date_unaware = parsed_date.replace(tzinfo=None)  #strips time zone info to allow arithmetic operations with 'now'
 
 
-			#check if the article was published recently
-			published_recently = (datetime.timedelta(minutes=10) > (now - parsed_date_unaware))
-			if published_recently:
-				links_to_submit.append([article["title"], article["link"]])
+		#check if the article was published recently
+		published_recently = (datetime.timedelta(minutes=10) > (now - parsed_date_unaware))
+		if published_recently:
+			links_to_submit.append([article["title"], article["link"]])
 
 			
 		
@@ -69,12 +69,11 @@ while True:
 
 
 
-	for item in links_to_submit:
-		print item[0]
-		print item[1]
-		reddit_instance.submit(subreddit="pythonforengineers", url=item[1], title=item[0], send_replies=False, resubmit=False)
+for item in links_to_submit:
+	print item[0]
+	print item[1]
+	reddit_instance.submit(subreddit="pythonforengineers", url=item[1], title=item[0], send_replies=False, resubmit=False)
 	
-	if ( len(links_to_submit) == 0):
-		print "No new articles."
+if ( len(links_to_submit) == 0):
+	print "No new articles."
 	#waits to loop again 
-	time.sleep(600)
